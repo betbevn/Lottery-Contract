4e1e2286ec60c811c1729a83d1d8cfab0bddd0a4
Hoang Nguyen <royal@Hoangs-MBP.lan>
2022-05-29 22:05:58 +0700

    add mocha test

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -10,18 +10,15 @@ contract Lottery {
 
     function enter() public payable {
         require(msg.value > .01 ether);
-
         players.push(msg.sender);
     }
 
-
-    function random() private view returns (uint) {
-        return uint(keccak256(block.difficulty, now, players));
+    function random() private view returns (uint256) {
+        return uint256(keccak256(block.difficulty, now, players));
     }
 
-
     function pickWinner() public restricted {
-        uint index = random() % players.length;
+        uint256 index = random() % players.length;
         players[index].transfer(this.balance);
         players = new address[](0);
     }
@@ -32,6 +29,6 @@ contract Lottery {
     }
 
     function getPlayers() public view returns (address[]) {
-        return players
+        return players;
     }
 }
--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -2,23 +2,21 @@ const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
+
 const { interface, bytecode } = require("../compile");
 
-let accounts;
 let lottery;
+let accounts;
 
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
+
   lottery = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({
-      data: bytecode,
-    })
+    .deploy({ data: bytecode })
     .send({ from: accounts[0], gas: "1000000" });
 });
-
-describe("Test", () => {
-  it("1", async () => {});
-  it("2", async () => {});
-  it("3", async () => {});
-  it("4", async () => {});
+describe("Lottery Contract", () => {
+  it("deploys a contract", () => {
+    assert.ok(lottery.options.address);
+  });
 });
