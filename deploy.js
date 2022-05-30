80c5d7cc65eeda37af6432cd354fea0678f3e728
Hoang Nguyen <royal@Hoangs-MacBook-Pro.local>
2022-05-30 14:58:31 +0700

    Add readme

--- deploy.js
+++ deploy.js
@@ -19,7 +19,7 @@ const deploy = async () => {
     .deploy({ data: bytecode })
     .send({ gas: "1000000", from: accounts[0] });
 
-  console.log("COntract deployed to", result.options.address);
+  console.log("Contract deployed to", result.options.address);
   provider.engine.stop();
 };
 deploy();
